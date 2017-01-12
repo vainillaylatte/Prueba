8c986f26ed2f14e136fd84b06fea074fba9dcfb7
vainillaylatte <dbfr312@gmail.com>
2017-01-11 21:13:21 -0600

    Create gestures.js

---
+++ gestures.js
@@ -0,0 +1,46 @@
+var app={
+	inicio: function(){
+		this.iniciaBotones();
+		this.iniciaFastClick();
+		this.iniciaHammer();
+	}
+	
+	iniciaFastClick: function () {
+		FastClick.attach(document.body);
+	}
+	
+	iniciaBotones: function(){
+		var botonClaro = document.querySelector('#claro');
+		var botonOscuro = document.querySelector('#oscuro');
+		
+		botonClaro.addEventListener('click',this.ponloClaro,false);
+		botonOscuro.addEventListener('click',this.ponloOscuro,false);
+	}
+	
+	iniciaHammer: function (){
+		var zona = document.getElementById('zona-gestos');
+		var hammertime = new Hammer(zona);
+		
+		hammertime.get('pinch').set[{ enable: true }];
+		hammertime.get('rotate').set[{ enable: true}];
+		
+		hammertime.on('tap doubletap pan swipe press pinch rotate', function(ev) {
+			document.querySelector('#info').innerHTML= ev.type+'!';
+		});
+	}
+	
+	ponloClaro: function(){
+		document.body.className = 'claro';
+	}
+	
+	ponloOscuro: function(){
+		document.body.className = 'oscuro';
+	}
+	
+};
+
+if ('addEventListener' in document) {
+	document.addEventListener('DOMContentLoaded', function() {
+		app.inicio();
+	}, false);
+}
